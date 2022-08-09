fdd24967388cdacb028debbf62f1af9ab5cbb8ed
Natalia Gomes <73845447+natalia248@users.noreply.github.com>
2022-08-10 00:52:49 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.querySelectorAll(".tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.querySelectorAll(".tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
